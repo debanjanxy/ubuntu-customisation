03af75971c661ac60142f7bde70980257939c1d2
debanjanxy <dasdebanjan624@gmail.com>
2018-02-13 01:19:46 +0530

    First Commit

---
+++ themes.sh
@@ -0,0 +1,22 @@
+#papirus icon theme
+sudo add-apt-repository ppa:varlesh-l/papirus-pack
+sudo apt-get update
+sudo apt-get install papirus-gtk-icon-theme
+
+#Install Arc Theme on Ubuntu and Linux Mint:
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install arc-theme
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install arc-icons
+
+#square-icons
+sudo add-apt-repository ppa:noobslab/icons2
+sudo apt-get update
+sudo apt-get install square-icons
+
+#paper theme
+sudo add-apt-repository ppa:snwh/pulp
+sudo apt-get update
+sudo apt-get install paper-gtk-theme paper-icon-theme
